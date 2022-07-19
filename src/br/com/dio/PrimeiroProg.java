08783ac0bd6762cf15c6bed818ef6359f3660c19
SouSuh <suellen-kouhai@hotmail.com>
2022-07-19 18:57:54 -0300

    commit remoção dos comentários

--- src/br/com/dio/PrimeiroProg.java
+++ src/br/com/dio/PrimeiroProg.java
@@ -9,13 +9,6 @@ public class PrimeiroProg {
 	Livros livros = new Livros();
 	System.out.println(gato);
 	System.out.println(livros);
-		
-		/*int a = 2;
-		int b = 5;
-		System.out.println("Hello N00b!!");
-		System.out.println("A= "+a + " B= "+b);
-		System.out.println("Somando... "+ a + " + " + b + " = " + (a+b));
-		System.out.println("Bye N00b!!");*/
 	}
 
 }
